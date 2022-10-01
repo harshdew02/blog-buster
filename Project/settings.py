b6e1a8ee65e9609e0595fb1b38bece5f6bd46751
Kraniket901 <kraniket123654@gmail.com>
2022-10-01 19:33:32 +0000

    updated settings

--- Project/settings.py
+++ Project/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u4gzelo6*46@p-1y48&4ia#sg^2_u6dt(ft$)4j&t+t*-8d6rt'
+# SECRET_KEY = 'django-insecure-u4gzelo6*46@p-1y48&4ia#sg^2_u6dt(ft$)4j&t+t*-8d6rt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-buster.herokuapp.com']
 
 
 # Application definition

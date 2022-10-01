b5d273fa0136d13076c993d5c3d642071ff2ddf6
Kraniket901 <kraniket123654@gmail.com>
2022-10-01 18:54:32 +0000

    Initial Commit

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView)
+from . import views
+
+
+urlpatterns = [
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
+    re_path(r'^users/(?P<username>\w{0,50})/$', UserPostListView.as_view(), name='user-posts'),
+    path('about/', views.about, name="blog-about"),
+]
